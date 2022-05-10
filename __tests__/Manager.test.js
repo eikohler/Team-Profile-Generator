1e9f5ceace44232817a5c387c85933e26d418f45
Eric Kohler <erickohler1600@gmail.com>
2022-05-10 11:48:15 -0400

    Create manager class, pass all tests

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require('../lib/Manager');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Dave', 123, 'dave@hotmail.com', 5);
+  
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toEqual(123);
+    expect(manager.email).toBe('dave@hotmail.com');
+    expect(manager.officeNumber).toEqual(5);
+});
+
+test('gets manager role', () => {
+    const manager = new Manager('Dave', 123, 'dave@hotmail.com', 5);
+  
+    expect(manager.getRole()).toBe('Manager');
+});

75f1e6f6693227d0c6f48daeb8cdd7eddf304b45
Eric Kohler <erickohler1600@gmail.com>
2022-05-10 12:03:39 -0400

    initial page template

---
+++ src/page-template.js
@@ -0,0 +1,21 @@
+module.exports = templateData => {
+  
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link rel="stylesheet" href="style.css">
+    </head>
+  
+    <body>
+      
+    </body>
+    </html>
+    `;
+};

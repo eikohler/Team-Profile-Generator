1d903dd8668a5a1a2f65c0080ff31d51273d4f89
Eric Kohler <erickohler1600@gmail.com>
2022-05-11 17:19:59 -0400

    prompts build employee objects and fill array of employees to be traversed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -9,6 +9,12 @@ test('creates a manager object', () => {
     expect(manager.officeNumber).toEqual(5);
 });
 
+test('gets manager office number', () => {
+    const manager = new Manager('Dave', 123, 'dave@hotmail.com', 5);
+  
+    expect(manager.getOfficeNum()).toEqual(5);
+});
+
 test('gets manager role', () => {
     const manager = new Manager('Dave', 123, 'dave@hotmail.com', 5);
   
--- index.js
+++ index.js
@@ -2,57 +2,67 @@ const generatePage = require('./src/page-template');
 
 const inquirer = require('inquirer');
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
 const promptManager = () => {
+
+    let profileData = [];
+
     return inquirer.prompt([
       {
         type: 'input',
-        name: 'managerName',
+        name: 'name',
         message: 'Enter the team manager’s name',
         validate: nameInput => {
-          if (nameInput) {
+          if (isNaN(nameInput) && nameInput) {
             return true;
           } else {
-            console.log('Please enter the team manager’s name');
-            return false;
+            return 'Please enter the team manager’s name';
           }
         }
       },
       {
         type: 'input',
-        name: 'managerID',
+        name: 'id',
         message: 'Enter the team manager’s ID',
         validate: (idInput) => {
-            if (isNaN(idInput)) {
-              return "Invalid entry, ID must be a number";
-            }
-            return true;
-        }
-      },
-      {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'Enter the team manager’s email',
-        validate: emailInput => {
-          if (emailInput) {
-            return true;
-          } else {
-            console.log('Please enter the team manager’s email');
-            return false;
+          if (!isNaN(idInput) && idInput) {
+            return true;              
+          }else{
+            return "Invalid entry, ID must be a number";
           }
         }
       },
       {
         type: 'input',
-        name: 'managerOfficeNum',
-        message: 'Enter the team manager’s office number',
-        validate: (ofNumInput) => {
-            if (isNaN(ofNumInput)) {
-              return "Invalid entry, office number must be a number";
-            }
+        name: 'email',
+        message: 'Enter the team manager’s email',
+        validate: emailInput => {
+          if (isNaN(emailInput) && emailInput) {
             return true;
+          } else {            
+            return 'Please enter the team manager’s email';
+          }
         }
       },
-    ]);
+      {
+        type: 'input',
+        name: 'officenum',
+        message: 'Enter the team manager’s office number',
+        validate: ofNumInput => {
+            if (!isNaN(ofNumInput) && ofNumInput) {
+              return true;              
+            }else{              
+              return "Invalid entry, office number must be a number";
+            }
+        }
+      },
+    ]).then(data => {
+      profileData.push(new Manager(data.name, data.id, data.email, data.officenum));
+      return profileData;
+    });
 };
 
 const promptNext = profileData => {
@@ -65,11 +75,129 @@ const promptNext = profileData => {
       },      
     ]).then(choice => {
         if(choice.nextChoice == 'Add an Engineer'){
-            console.log('Engineer');
-        }else if(choice.nextChoice == 'Add an Engineer'){
-
+          return addEngineer(profileData);
+        }else if(choice.nextChoice == 'Add an Intern'){
+          return addIntern(profileData);
+        }else{
+          return profileData;
         }
     });
 };
 
-promptManager().then(promptNext);
+const addEngineer = profileData => {
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter the engineer’s name',
+      validate: nameInput => {
+        if (isNaN(nameInput) && nameInput) {
+          return true;
+        } else {
+          return 'Please enter the engineer’s name';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter the engineer’s ID',
+      validate: (idInput) => {
+        if (!isNaN(idInput) && idInput) {
+          return true;              
+        }else{
+          return "Invalid entry, ID must be a number";
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the engineer’s email',
+      validate: emailInput => {
+        if (isNaN(emailInput) && emailInput) {
+          return true;
+        } else {            
+          return 'Please enter the engineer’s email';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter the engineer’s Github username',
+      validate: github => {
+          if (isNaN(github) && github) {
+            return true;              
+          }else{              
+            return "Please enter the engineer’s Github username";
+          }
+      }
+    },
+  ]).then(data => {
+    profileData.push(new Engineer(data.name, data.id, data.email, data.github));
+    return promptNext(profileData);
+  });
+};
+
+const addIntern = profileData => {
+
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter the intern’s name',
+      validate: nameInput => {
+        if (isNaN(nameInput) && nameInput) {
+          return true;
+        } else {
+          return 'Please enter the intern’s name';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter the intern’s ID',
+      validate: (idInput) => {
+        if (!isNaN(idInput) && idInput) {
+          return true;              
+        }else{
+          return "Invalid entry, ID must be a number";
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter the intern’s email',
+      validate: emailInput => {
+        if (isNaN(emailInput) && emailInput) {
+          return true;
+        } else {            
+          return 'Please enter the intern’s email';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Enter the intern’s school',
+      validate: school => {
+          if (isNaN(school) && school) {
+            return true;              
+          }else{              
+            return "Please enter the intern’s school";
+          }
+      }
+    },
+  ]).then(data => {
+    profileData.push(new Intern(data.name, data.id, data.email, data.school));
+    return promptNext(profileData);
+  });
+};
+
+promptManager().then(promptNext).then(profileData =>{
+  console.log(profileData);
+});
